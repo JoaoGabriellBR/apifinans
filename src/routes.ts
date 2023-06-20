64f1b894650d8fd381c6edd7bff6d302d04f2f80
João Gabriel <joaoname9@gmail.com>
2023-06-20 17:32:52 -0300

    refactor routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+
 import User from "./modules/User";
 import Bill from "./modules/Bill";
 import Expense from "./modules/Expense";
-// import Revenue from "./modules/Revenue";
+import Revenue from "./modules/Revenue";
+
 import decodeUserToken from "./middlewares";
 
 const routes = Router();
@@ -19,34 +21,26 @@ routes.patch("/user/change-password", User.changePassword);
 
 routes.post("/bill/create", Bill.createBill);
 // routes.get("/bill/get/:id", Bill.getBill);
-
-
-// RETIRAR O /:ID DE /bill/get/:id NA FUNÇÃO ABAIXO, POIS ELE SERÁ PEGO DE USERDATA.ID
-routes.get("/bill/get/:id", Bill.getAllBills);
-
+routes.get("/bill/get", Bill.getAllBills);
 routes.patch("/bill/update/:id", Bill.updateBill);
 routes.patch("/bill/delete/:id", Bill.deleteBill);
 
 // EXPENSES
 
-routes.post("/expense/create/:id", Expense.createExpense);
+routes.post("/expense/create", Expense.createExpense);
 routes.patch("/pay-expense/:id", Expense.payExpense);
-// routes.get("/expense/get/:id", Expense.getExpense);
-
-// RETIRAR O /:ID DE /expense/get/:id NA FUNÇÃO ABAIXO, POIS ELE SERÁ PEGO DE USERDATA.ID
-routes.get("/expense/get/:id", Expense.getAllExpenses);
-
-
-// routes.patch("/expense/update/:id", Expense.updateExpense);
-// routes.patch("/expense/delete/:id", Expense.deleteExpense);
+routes.get("/expense/get/:id", Expense.getExpense);
+routes.get("/expense/get", Expense.getAllExpenses);
+routes.patch("/expense/update/:id", Expense.updateExpense);
+routes.patch("/expense/delete/:id", Expense.deleteExpense);
 
 // REVENUES
 
-// routes.post("/revenue/create", Revenue.createBill);
-// routes.get("/revenue/get/:id", Revenue.getRevenue);
-// routes.get("/revenue/get", Revenue.getAllRevenues);
-// routes.patch("/revenue/update/:id", Revenue.updateRevenue);
-// routes.patch("/revenue/delete/:id", Revenue.deleteRevenue);
+routes.post("/revenue/create", Revenue.createRevenue);
+routes.get("/revenue/get/:id", Revenue.getRevenue);
+routes.get("/revenue/get", Revenue.getAllRevenues);
+routes.patch("/revenue/update/:id", Revenue.updateRevenue);
+routes.patch("/revenue/delete/:id", Revenue.deleteRevenue);
 
 export default routes;
 
