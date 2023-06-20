a4b8e5a27d9068e49de31e6414f951d5844e6d81
João Gabriel <joaoname9@gmail.com>
2023-06-20 16:28:10 -0300

    add try catch block Expense functions

--- src/modules/Expense/index.ts
+++ src/modules/Expense/index.ts
@@ -18,147 +18,183 @@ const userWithoutPassword = {
 
 export = {
   async createExpense(req: Request, res: Response) {
-    const { userData } = req;
-    const { value, description, status } = req.body;
-    const { id } = req.params; // id da conta;
+    try {
+      const { userData } = req;
+      const { value, description, status } = req.body;
+      const { id } = req.params; // id da conta;
 
-    if (!value || !description)
-      return res
-        .status(400)
-        .send({ error: "Forneça todos os dados solicitados!" });
+      if (!value || !description)
+        return res
+          .status(400)
+          .send({ error: "Forneça todos os dados solicitados!" });
 
-    const response = await prisma.tb_expense.create({
-      data: {
-        value,
-        description,
-        status: !!status,
-        author: { connect: { id: 1 } },
-        // author: { connect: { id: userData.id } },
-        tb_bill: { connect: { id: parseInt(id) } },
-      },
-      include: { author: { select: userWithoutPassword } },
-    });
+      const response = await prisma.tb_expense.create({
+        data: {
+          value,
+          description,
+          status: !!status,
+          author: { connect: { id: 1 } },
+          // author: { connect: { id: userData.id } },
+          tb_bill: { connect: { id: parseInt(id) } },
+        },
+        include: { author: { select: userWithoutPassword } },
+      });
 
-    res.status(201).send({ success: true, response });
+      res.status(201).send({ success: true, response });
+    } catch (error: any) {
+      console.log(error?.response?.data);
+      res.status(500).send({ error: "Ocorreu um erro ao criar a despesa." });
+    }
   },
 
   async payExpense(req: Request, res: Response) {
-    const { userData } = req;
-    const { id } = req.params;
+    try {
+      const { userData } = req;
+      const { id } = req.params;
 
-    const expenseExists = await prisma.tb_expense.findUnique({
-      where: { id: parseInt(id) },
-    });
+      const expenseExists = await prisma.tb_expense.findUnique({
+        where: { id: parseInt(id) },
+      });
 
-    if (!expenseExists || expenseExists.deleted_at !== null) {
-      return res.status(404).send({ error: "Despesa não encontrada." });
+      if (!expenseExists || expenseExists.deleted_at !== null) {
+        return res.status(404).send({ error: "Despesa não encontrada." });
+      }
+
+      await prisma.tb_expense.update({
+        where: { id: parseInt(id) },
+        data: { status: true },
+      });
+
+      res
+        .status(200)
+        .send({ success: true, message: "Despesa paga com sucesso." });
+    } catch (error: any) {
+      console.log(error?.response?.data);
+      res.status(500).send({
+        error: "Ocorreu um erro ao confirmar o pagamento da despesa.",
+      });
     }
-
-    await prisma.tb_expense.update({
-      where: { id: parseInt(id) },
-      data: { status: true },
-    });
-
-    res
-      .status(200)
-      .send({ success: true, message: "Despesa paga com sucesso." });
   },
 
   async getExpense(req: Request, res: Response) {
-    const { userData } = req;
-    const { id } = req.params;
+    try {
+      const { userData } = req;
+      const { id } = req.params;
 
-    const expenseExists = await prisma.tb_expense.findUnique({
-      where: { id: parseInt(id) },
-    });
+      const expenseExists = await prisma.tb_expense.findUnique({
+        where: { id: parseInt(id) },
+      });
 
-    if (!expenseExists || expenseExists.deleted_at !== null) {
-      return res.status(404).send({ error: "Despesa não encontrada." });
+      if (!expenseExists || expenseExists.deleted_at !== null) {
+        return res.status(404).send({ error: "Despesa não encontrada." });
+      }
+
+      const response = await prisma.tb_expense.findFirst({
+        where: {
+          id: expenseExists.id,
+          deleted_at: null,
+        },
+        include: {
+          author: { select: userWithoutPassword },
+        },
+      });
+
+      res.status(200).json({ success: true, response });
+    } catch (error: any) {
+      console.log(error?.response?.data);
+      res.status(500).send({ error: "Ocorreu um erro ao carregar a despesa." });
     }
-
-    const response = await prisma.tb_expense.findFirst({
-      where: {
-        id: expenseExists.id,
-        deleted_at: null,
-      },
-      include: {
-        author: { select: userWithoutPassword },
-      },
-    });
-
-    res.status(200).json({ success: true, response });
   },
 
   async getAllExpenses(req: Request, res: Response) {
-    const { userData } = req;
-    const { id } = req.params;
+    try {
+      const { userData } = req;
+      const { id } = req.params;
 
-    const response = await prisma.tb_expense.findMany({
-      where: {
-        // id_author: userData?.id,
-        id: parseInt(id),
-        deleted_at: null,
-      },
-      orderBy: { created_at: "desc" },
-      include: {
-        author: { select: userWithoutPassword },
-      },
-    });
+      const response = await prisma.tb_expense.findMany({
+        where: {
+          // id_author: userData?.id,
+          id: parseInt(id),
+          deleted_at: null,
+        },
+        orderBy: { created_at: "desc" },
+        include: {
+          author: { select: userWithoutPassword },
+        },
+      });
 
-    res.status(200).send({ success: true, response });
+      res.status(200).send({ success: true, response });
+    } catch (error: any) {
+      console.log(error?.response?.data);
+      res
+        .status(500)
+        .send({ error: "Ocorreu um erro ao carregar as despesas." });
+    }
   },
 
   async updateExpense(req: Request, res: Response) {
-    const { userData } = req;
-    const { id } = req.params;
-    const { value, description, status } = req.body;
+    try {
+      const { userData } = req;
+      const { id } = req.params;
+      const { value, description, status } = req.body;
 
-    const expenseExists = await prisma.tb_expense.findUnique({
-      where: { id: parseInt(id) },
-    });
+      const expenseExists = await prisma.tb_expense.findUnique({
+        where: { id: parseInt(id) },
+      });
 
-    if (!expenseExists || expenseExists.deleted_at !== null) {
-      return res.status(404).send({ error: "Despesa não encontrada." });
+      if (!expenseExists || expenseExists.deleted_at !== null) {
+        return res.status(404).send({ error: "Despesa não encontrada." });
+      }
+
+      const response = await prisma.tb_expense.update({
+        where: { id: expenseExists.id },
+        data: {
+          value,
+          description,
+          status: !!status,
+          // author: { connect: { id: userData.id } },
+          author: { connect: { id: 1 } },
+        },
+        include: {
+          author: { select: userWithoutPassword },
+        },
+      });
+
+      res.status(200).send({ success: true, response });
+    } catch (error: any) {
+      console.log(error?.response?.data);
+      res
+        .status(500)
+        .send({ error: "Ocorreu um erro ao atualizar a despesa." });
     }
-
-    const response = await prisma.tb_expense.update({
-      where: { id: expenseExists.id },
-      data: {
-        value,
-        description,
-        status: !!status,
-        // author: { connect: { id: userData.id } },
-        author: { connect: { id: 1 } }
-      },
-      include: {
-        author: { select: userWithoutPassword },
-      },
-    });
-
-    res.status(200).send({ success: true, response });
   },
 
   async deleteExpense(req: Request, res: Response) {
-    const { userData } = req;
-    const { id } = req.params;
+    try {
+      const { userData } = req;
+      const { id } = req.params;
 
-    const expenseExists = await prisma.tb_expense.findUnique({
-      where: { id: parseInt(id) },
-    });
+      const expenseExists = await prisma.tb_expense.findUnique({
+        where: { id: parseInt(id) },
+      });
 
-    if (!expenseExists || expenseExists.deleted_at !== null) {
-      return res.status(404).send({ error: "Despesa não encontrada." });
+      if (!expenseExists || expenseExists.deleted_at !== null) {
+        return res.status(404).send({ error: "Despesa não encontrada." });
+      }
+
+      await prisma.tb_expense.update({
+        where: { id: expenseExists.id },
+        data: {
+          deleted_at: new Date(),
+        },
+      });
+
+      res
+        .status(200)
+        .send({ success: true, message: "Despesa excluída com sucesso." });
+    } catch (error: any) {
+      console.log(error?.response?.data);
+      res.status(500).send({ error: "Ocorreu um erro ao excluír a despesa." });
     }
-
-    await prisma.tb_expense.update({
-      where: { id: expenseExists.id },
-      data: {
-        deleted_at: new Date(),
-      },
-    });
-
-    res
-      .status(200)
-      .send({ success: true, message: "Despesa excluída com sucesso." });
   },
 };
