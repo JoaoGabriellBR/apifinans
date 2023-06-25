98b9cfc30ec7f2f14d3ba184eaa913ed75d023bd
João Gabriel <joaoname9@gmail.com>
2023-06-25 01:21:44 -0300

    refactor middlewares and fix bugs

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,28 +1,26 @@
-import jwt, { Secret } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload, DecodeOptions } from "jsonwebtoken";
+import generateSecretKey from "../utils/secretKey";
 
-declare module "express" {
-  interface Request {
-    userData?: any;
-  }
+const secretKey = generateSecretKey(32);
+
+interface CustomRequest extends Request {
+  userData?: JwtPayload;
 }
 
-const secret = process.env.AUTH_SECRET;
-
-export default async function decodeUserToken(
-  req: Request,
+async function decodeUserToken(
+  req: CustomRequest,
   res: Response,
   next: NextFunction
 ) {
   const { authorization } = req.headers;
+  if (!authorization) return res.status(401).send({ error: "Unauthorized." });
 
-  if (!authorization) return res.status(401).send({ error: "Unauthorized" });
+  const decodedToken = jwt.decode(authorization, secretKey as DecodeOptions);
+  if (!decodedToken) return res.status(401).send({ error: "Unauthorized." });
 
-  try {
-    const decodedToken = jwt.verify(authorization, secret as Secret);
-    req.userData = decodedToken;
-    next();
-  } catch (error) {
-    return res.status(401).send({ error: "Unauthorized" });
-  }
+  req.userData = decodedToken as JwtPayload;
+  next();
 }
+
+export default decodeUserToken;
