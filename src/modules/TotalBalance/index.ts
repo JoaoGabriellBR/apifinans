bae8c0b1381898560b469ace9a7ffe21573e9b37
João Gabriel <joaoname9@gmail.com>
2023-07-04 16:49:16 -0300

    feat: adding status in totaBalanceBill

--- src/modules/TotalBalance/index.ts
+++ src/modules/TotalBalance/index.ts
@@ -28,25 +28,32 @@ export = {
           revenues: { where: notDeleted },
         },
       });
-    
+
       const totalBalanceBill = bills.reduce((total, bill) => {
-        
-        const expensesBalance = bill.expenses.reduce(
-          (total, expense) => total + (expense.balance ?? 0),
-          0
-        );
+        const expensesBalance = bill.expenses.reduce((total, expense) => {
+          if (expense.status) {
+            return total + (expense.balance ?? 0);
+          } else {
+            return total;
+          }
+        }, 0);
 
-        const revenuesBalance = bill.revenues.reduce(
-          (total, revenue) => total + (revenue.balance ?? 0),
-          0
-        );
+        const revenuesBalance = bill.revenues.reduce((total, revenue) => {
+          if (revenue.status) {
+            return total + (revenue.balance ?? 0);
+          } else {
+            return total;
+          }
+        }, 0);
 
-        const billTotal = (bill.balance ?? 0) + revenuesBalance - expensesBalance;
+        const billTotal =
+          (bill.balance ?? 0) + revenuesBalance - expensesBalance;
         return total + billTotal;
       }, 0);
-    
-      res.status(200).send({ success: true, total_balance_bill: totalBalanceBill });
 
+      res
+        .status(200)
+        .send({ success: true, total_balance_bill: totalBalanceBill });
     } catch (error: any) {
       console.log(error.message);
       res.status(500).send({ error: "Ocorreu um erro ao carregar os dados." });
